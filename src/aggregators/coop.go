882db9ec639f67e3b9e78ce415bb604463163830
Amit Lavon <doctor_troll@walla.co.il>
2015-06-20 22:43:52 +0300

    started a coop aggregator

---
+++ src/aggregators/coop.go
@@ -0,0 +1,32 @@
+package aggregators
+
+// An aggregator for the Co-Op chain.
+
+import (
+	"net/http"
+	"io"
+	"fmt"
+	"os"
+)
+
+type CoopAggregator struct{}
+
+// Returns a new Co-Op aggregator.
+func NewCoopAggregator() *CoopAggregator {
+	return &CoopAggregator{}
+}
+
+func (a *CoopAggregator) Aggregate(dir string) error {
+	jar, _ := singleCookieJar("http://coopisrael.coop",
+			"_gat", "GA1.2.1410125988.1434778651")
+	cl := &http.Client{Jar: jar}
+	// cl := &http.Client{}
+	res, err := cl.Get("http://coopisrael.coop/home/get_promo/?branch=2&type=gzip&agree=1")
+	if err != nil { return err }
+	defer res.Body.Close()
+	
+	fmt.Println(res.Status)
+	io.Copy(os.Stdout, res.Body)
+
+	return nil
+}
