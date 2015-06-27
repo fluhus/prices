4b9a2cb85d307186d53916216faaebe21213cb16
Amit Lavon <amitlavon@cs.huji.ac.il>
2015-06-27 18:38:39 +0300

    more items parser

--- src/items/items.go
+++ src/items/items.go
@@ -9,7 +9,6 @@ import (
 )
 
 func main() {
-	makeCaptures()
 	pe("Parses price XMLs to TSV tables.")
 	pe("Reading from stdin...")
 	
@@ -20,19 +19,17 @@ func main() {
 		os.Exit(2)
 	}
 	
-	// Parse fields.
+	// Parse items.
 	items := captureXml("Item").FindAllSubmatch(data, -1)
 	pef("Found %d items.\n", len(items))
 	for i := range items {
 		item := items[i][1]
-		for _, re := range fieldRes {
-			field := re.FindSubmatch(item)
-			if field != nil {
-				fmt.Printf("%s", field[1])
+		for _, f := range fields {
+			value := f.re().FindSubmatch(item)
+			if f.mandatory && (value == nil || len(value[1]) == 0) {
+				pef("Error: No value for mandatory field '%s'.", f.tag)
 			}
-			fmt.Printf("\t")
 		}
-		fmt.Printf("\n")
 	}
 }
 
@@ -46,34 +43,43 @@ func pef(s string, a ...interface{}) {
 	fmt.Fprintf(os.Stderr, s, a...)
 }
 
-var fields = []string {
-	"PriceUpdateDate",
-	"ItemCode",
-	"ItemType",
-	"ItemName",
-	"ManufacturerName",
-	"ManufacturerCountry",
-	"ManufacturerItemDescription",
-	"UnitQty",
-	"Quantity",
-	"UnitOfMeasure",
-	"b(?:I|l)sWeighted",
-	"QtyInPackage",
-	"ItemPrice",
-	"UnitOfMeasurePrice",
-	"AllowDiscount",
-	"ItemStatus",
+type field struct {
+	tag string
+	global bool
+	mandatory bool
+	rgx *regexp.Regexp
 }
 
-var fieldRes []*regexp.Regexp
+func (f field) re() *regexp.Regexp {
+	if f.rgx == nil {
+		f.rgx = captureXml(f.tag)
+	}
+	return f.rgx
+}
+
+var fields = []*field {
+	&field{"PriceUpdateDate", false, true, nil},
+	&field{"ItemCode", false, true, nil},
+	&field{"ItemType", false, false, nil},
+	&field{"ItemName", false, true, nil},
+	&field{"ManufacturerName", false, false, nil},
+	&field{"ManufacturerCountry", false, false, nil},
+	&field{"ManufacturerItemDescription", false, false, nil},
+	&field{"UnitQty", false, false, nil},
+	&field{"Quantity", false, false, nil},
+	&field{"UnitOfMeasure", false, false, nil},
+	&field{"b(?:I|l)sWeighted", false, false, nil},
+	&field{"QtyInPackage", false, false, nil},
+	&field{"ItemPrice", false, true, nil},
+	&field{"UnitOfMeasurePrice", false, false, nil},
+	&field{"AllowDiscount", false, false, nil},
+	&field{"ItemStatus", false, false, nil},
+}
 
 func captureXml(tag string) *regexp.Regexp {
 	return regexp.MustCompile("(?si)<" + tag + ">(.*?)</" + tag + ">")
 }
 
-func makeCaptures() {
-	fieldRes = make([]*regexp.Regexp, len(fields))
-	for i := range fields {
-		fieldRes[i] = captureXml(fields[i])
-	}
-}
+
+
+
