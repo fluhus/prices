41e03e7c1cbc3d30b0206e4585ea4a98e2824033
Amit Lavon <amitlavon@cs.huji.ac.il>
2015-06-27 12:48:34 +0300

    more item parser

--- src/items/items.go
+++ src/items/items.go
@@ -9,64 +9,36 @@ import (
 )
 
 func main() {
-	pef("Parses price XMLs to TSV tables.\n")
-	pef("Reading from stdin...\n")
+	makeCaptures()
+	pe("Parses price XMLs to TSV tables.")
+	pe("Reading from stdin...")
 	
 	// Read input XML.
 	data, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		pef("Error reading input: %v\n", err)
+		pe("Error reading input:", err)
 		os.Exit(2)
 	}
 	
 	// Parse fields.
-	fields := []string{
-		"Chainid",
-		"SubChainid",
-		"Storeid",
-		"BikoretNo",
-		"PriceUpdateDate",
-		"PriceUpdateTime",
-		"ItemCode",
-		"ItemType",
-		"ItemName",
-		"ManufacturerName",
-		"ManufacturerItemCountry",
-		"ManufacturerItemDescription",
-		"UnitQty",
-		"Quantity",
-		"UnitOfMeasure",
-		"BlsWeighted",
-		"QtyInPackage",
-		"ItemPrice",
-		"UnitOfMeasurePrice",
-		"AllowDiscount",
-		"ItemStatus",
-	}
-	
-	match := fieldRegexp(fields).FindAllSubmatch(data, -1)
-
-	// Print findings.
-	for i := range fields {
-		if i == 0 {
-			fmt.Printf("%s", fields[i])
-		} else {
-			fmt.Printf("\t%s", fields[i])
-		}
-	}
-	fmt.Printf("\n")
-
-	for i := range match {
-		for j := range match[i] {
-			if j == 0 { continue }
-			if j == 1 {
-				fmt.Printf("%s", match[i][j])
-			} else {
-				fmt.Printf("\t%s", match[i][j])
+	items := captureXml("Item").FindAllSubmatch(data, -1)
+	pef("Found %d items.\n", len(items))
+	for i := range items {
+		item := items[i][1]
+		for _, re := range fieldRes {
+			field := re.FindSubmatch(item)
+			if field != nil {
+				fmt.Printf("%s", field[1])
 			}
+			fmt.Printf("\t")
 		}
 		fmt.Printf("\n")
 	}
+}
+
+// Println to stderr.
+func pe(a ...interface{}) {
+	fmt.Println(a...)
 }
 
 // Printf to stderr.
@@ -74,10 +46,34 @@ func pef(s string, a ...interface{}) {
 	fmt.Fprintf(os.Stderr, s, a...)
 }
 
-func fieldRegexp(fields []string) *regexp.Regexp {
-	re := "(?s)"
-	for _, field := range fields {
-		re += fmt.Sprintf(".*?<%s>(.*?)</%s>", field, field)
+var fields = []string {
+	"PriceUpdateDate",
+	"ItemCode",
+	"ItemType",
+	"ItemName",
+	"ManufacturerName",
+	"ManufacturerCountry",
+	"ManufacturerItemDescription",
+	"UnitQty",
+	"Quantity",
+	"UnitOfMeasure",
+	"b(?:I|l)sWeighted",
+	"QtyInPackage",
+	"ItemPrice",
+	"UnitOfMeasurePrice",
+	"AllowDiscount",
+	"ItemStatus",
+}
+
+var fieldRes []*regexp.Regexp
+
+func captureXml(tag string) *regexp.Regexp {
+	return regexp.MustCompile("(?si)<" + tag + ">(.*?)</" + tag + ">")
+}
+
+func makeCaptures() {
+	fieldRes = make([]*regexp.Regexp, len(fields))
+	for i := range fields {
+		fieldRes[i] = captureXml(fields[i])
 	}
-	return regexp.MustCompile(re)
 }
