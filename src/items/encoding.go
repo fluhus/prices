f0db1be28ba18a17c73bd4a4b67c92e652b9bf91
Amit Lavon <amitlavon@cs.huji.ac.il>
2015-10-07 14:51:00 +0300

    items now escapes stray ampersands

--- src/items/encoding.go
+++ src/items/encoding.go
@@ -5,14 +5,13 @@ package main
 import (
 	"bytes"
 	"regexp"
-	"os/exec"
 	"io/ioutil"
 	"golang.org/x/net/html/charset"
 )
 
 // Converts the given XML to utf-8.
 func toUtf8(text []byte) ([]byte, error) {
-	// Charset reader converts arbitrary text to UTF-8.
+	// Charset reader converts arbitrary text to UTF-8. Hurray!
 	r, err := charset.NewReader(bytes.NewBuffer(text), "application/xml")
 	if err != nil {
 		return nil, err
@@ -26,6 +25,11 @@ func toUtf8(text []byte) ([]byte, error) {
 	// Replace encoding field with utf-8.
 	newText = regexp.MustCompile("encoding=\".*?\"").ReplaceAll(newText,
 			[]byte("encoding=\"utf-8\""))
+	
+	// Escape ampersands that are not part of an escape sequence (&...;).
+	// In some chains they forgot to escape them and it annoys the XML parser.
+	newText = regexp.MustCompile("&([a-z]*[^a-z;])").ReplaceAll(
+			newText, []byte("&amp;$1"))
 	
 	return newText, nil
 }
