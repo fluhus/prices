227f198fec2d26b661b001f20766e6eec8c3d239
Amit Lavon <amitlavon@cs.huji.ac.il>
2015-10-07 14:06:25 +0300

    improved encoding convertion in items

--- src/items/encoding.go
+++ src/items/encoding.go
@@ -3,65 +3,22 @@ package main
 // Functionality for converting XML text encoding to UTF-8.
 
 import (
-	"fmt"
+	"bytes"
 	"regexp"
 	"os/exec"
 	"io/ioutil"
+	"golang.org/x/net/html/charset"
 )
 
-// Converts the given XML to utf-8, according to its 'encoding' field. Uses
-// the shell command 'iconv' for the convertion. Also converts the field value
-// to utf-8.
+// Converts the given XML to utf-8.
 func toUtf8(text []byte) ([]byte, error) {
-	match := regexp.MustCompile("encoding=\"(.*?)\"").FindSubmatch(text)
-	
-	// No encoding is assumed to be utf-8.
-	if match == nil {
-		return text, nil
-	}
-	
-	// Handle encoding.
-	enc := string(match[1])
-	
-	if enc == "" {
-		return nil, fmt.Errorf("Encoding field is empty.")
-	}
-	
-	if enc == "utf-8" || enc == "UTF-8" {
-		return text, nil
-	}
-	
-	// Convert to utf-8.
-	cmd := exec.Command("iconv", "-f", enc, "-t", "utf-8")
-	out, err := cmd.StdoutPipe()
-	if err != nil {
-		return nil, err
-	}
-	in, err := cmd.StdinPipe()
+	// Charset reader converts arbitrary text to UTF-8.
+	r, err := charset.NewReader(bytes.NewBuffer(text), "application/xml")
 	if err != nil {
 		return nil, err
 	}
 	
-	err = cmd.Start()
-	if err != nil {
-		return nil, err
-	}
-	
-	// Push input to stdin.
-	go func() {
-		in.Write(text)
-		in.Close()
-	}()
-
-	// Read from stdout.
-	newText, err := ioutil.ReadAll(out)
-	if err != nil {
-		return nil, err
-	}
-	
-	// End command.
-	err = cmd.Wait()
-	
+	newText, err := ioutil.ReadAll(r)
 	if err != nil {
 		return nil, err
 	}
