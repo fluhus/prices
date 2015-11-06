859af53821125fe9f3fac78823cb3174653bb41f
Amit Lavon <amitlavon@cs.huji.ac.il>
2015-11-06 17:10:20 +0200

    items2 - removed use of regexps in correctxml, for speedup

--- src/items2/correctxml.go
+++ src/items2/correctxml.go
@@ -4,7 +4,6 @@ package main
 
 import (
 	"bytes"
-	"regexp"
 	"io/ioutil"
 	"golang.org/x/net/html/charset"
 )
@@ -12,37 +11,114 @@ import (
 // Converts the given XML to utf-8, and corrects some syntax errors that the
 // publishers make.
 func correctXml(text []byte) ([]byte, error) {
-	// Charset reader converts arbitrary text to UTF-8. Hurray!
-	r, err := charset.NewReader(bytes.NewBuffer(text), "application/xml")
-	if err != nil {
-		return nil, err
+	text = correctEncodingToUtf8(text)
+	text = correctGibberish(text)
+	text = correctUnquotedAttrs(text)
+	text = correctEncodingField(text)
+	text = correctAmpersands(text)
+	
+	return text, nil
+}
+
+// Encodes the given text in UTF-8.
+func correctEncodingToUtf8(text []byte) []byte {
+	r, _ := charset.NewReader(bytes.NewBuffer(text), "application/xml")
+	text, _ = ioutil.ReadAll(r)
+	return text
+}
+
+// Some Gibberish will not convert to UTF-8, so this function converts it
+// manually.
+func correctGibberish(text []byte) []byte {
+	for i := 0; i < len(text) - 1; i++ {
+		if text[i] == 195 && text[i+1] >= 160 && text[i+1] <= 186 {
+			text[i] = 215
+			text[i+1] -= 16
+		}
+	}
+	return text
+}
+
+// Replaces encoding attribute value with utf-8.
+func correctEncodingField(text []byte) []byte {
+	buf := bytes.NewBuffer(make([]byte, 0, len(text)*2))
+	
+	for i := 0; i < len(text); i++ {
+		buf.WriteByte(text[i])
+		
+		// If found an encoding field.
+		if text[i] == '=' && i >= 8 && string(text[i-8:i+1]) == "encoding=" {
+			buf.WriteString("\"utf-8\"")
+			
+			// Advance to after end of field.
+			i += 2
+			for text[i] != '"' {
+				i++
+			}
+		}
 	}
 	
-	newText, err := ioutil.ReadAll(r)
-	if err != nil {
-		return nil, err
+	return buf.Bytes()
+}
+
+// Quote unquoted attributes (Bitan has unquoted counts in their promo
+// files).
+func correctUnquotedAttrs(text []byte) []byte {
+	buf := bytes.NewBuffer(make([]byte, 0, len(text)*2))
+	
+	for i := 0; i < len(text); i++ {
+		buf.WriteByte(text[i])
+		
+		if text[i] == '=' && i >= 1 && i < len(text) - 1 &&
+				isLetter(text[i-1]) && isAlphaNum(text[i+1]) {
+			buf.WriteByte('"')
+			for i < len(text) - 1 && isAlphaNum(text[i+1]) {
+				i++
+				buf.WriteByte(text[i])
+			}
+			buf.WriteByte('"')
+		}
 	}
 	
-	// Some Gibberish will not convert, so convert manually.
-	for i := byte(160); i <= 186; i++ {
-		newText = bytes.Replace(newText,
-				[]byte{195, i}, []byte{215, i-16}, -1)
+	return buf.Bytes()
+}
+
+// Escapes ampersands that are not part of an escape sequence (&...;).
+// In some chains they forgot to escape them and it annoys the XML parser.
+func correctAmpersands(text []byte) []byte {
+	buf := bytes.NewBuffer(make([]byte, 0, len(text)*2))
+	
+	for i := 0; i < len(text); i++ {
+		buf.WriteByte(text[i])
+		
+		if text[i] == '&' && i < len(text) - 1 {
+			suffix := text[i+1:]
+			if suffix[0] == '#' {
+				continue
+			}
+			for j := range suffix {
+				if !isLetter(suffix[j]) {
+					if suffix[j] != ';' {
+						buf.WriteString("amp;")
+					}
+					break
+				}
+			}
+		}
 	}
 	
-	// Replace encoding field with utf-8.
-	newText = regexp.MustCompile("encoding=\".*?\"").ReplaceAll(newText,
-			[]byte("encoding=\"utf-8\""))
-	
-	// Escape ampersands that are not part of an escape sequence (&...;).
-	// In some chains they forgot to escape them and it annoys the XML parser.
-	newText = regexp.MustCompile("&([^#a-z]|[a-z]+[^a-z;])").ReplaceAll(
-			newText, []byte("&amp;$1"))
-	
-	// Quote unquoted attributes (Bitan has unquoted counts in their promo
-	// files).
-	newText = regexp.MustCompile("(\\w+=)(\\w+)").ReplaceAll(
-			newText, []byte("$1\"$2\""))
-	
-	return newText, nil
+	return buf.Bytes()
+}
+
+func isLetter(b byte) bool {
+	return (b >= 'A' && b <= 'Z') || (b >= 'a' && b <= 'z')
+}
+
+func isDigit(b byte) bool {
+	return b >= '0' && b <= '9'
+}
+
+func isAlphaNum(b byte) bool {
+	return isLetter(b) || isDigit(b)
 }
 
