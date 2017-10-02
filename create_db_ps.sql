a70731fc2704d7a9606ea2fcc43769d32969f344
amitl <amitl@amitl-lpt.roam.corp.google.com>
2017-10-02 19:53:42 +0300

    update psql create script

--- create_db_ps.sql
+++ create_db_ps.sql
@@ -2,9 +2,6 @@
 
 \connect postgres
 
--- Create new database.
-DROP DATABASE prices;
-CREATE DATABASE prices;
 COMMENT ON DATABASE prices IS
 'See XML specifications here:
 https://drive.google.com/file/d/0Bw2XXw9aHzlCT0xRMV9WSnZIS0E/view
@@ -21,10 +18,11 @@ Data fields that were created or curated by us are marked explicitly as safe.'
 \connect prices
 
 -- Create tables.
-CREATE TABLE chains (
+CREATE TABLE IF NOT EXISTS chains (
   chain_id   text PRIMARY KEY,
   chain_name text
 );
+DELETE FROM chains;
 COMMENT ON TABLE chains IS 'Maps chain code to chain name.';
 COMMENT ON COLUMN chains.chain_id IS 'Chain code, as provided by GS1. (safe)';
 COMMENT ON COLUMN chains.chain_name IS 'Chain name in English. (safe)';
@@ -50,12 +48,13 @@ INSERT INTO chains VALUES
 --('7290058140886','Zol VeBegadol')
 ;
 
-CREATE TABLE stores (
+CREATE TABLE IF NOT EXISTS stores (
   store_id           integer,
   chain_id           text  NOT NULL,
   subchain_id        text  NOT NULL,
   reported_store_id  text  NOT NULL
 );
+DELETE FROM stores;
 COMMENT ON TABLE stores IS 'Identifies every store in the data. Each store may appear once.';
 COMMENT ON COLUMN stores.store_id IS '(safe)';
 COMMENT ON COLUMN stores.chain_id IS 'Chain code, as provided by GS1.';
@@ -64,7 +63,7 @@ COMMENT ON COLUMN stores.reported_store_id IS 'Store number issued by the chain.
 
 COPY stores FROM '/home/amit/prices/data_parsed/stores.txt' WITH (FORMAT csv);
 
-CREATE TABLE stores_meta (
+CREATE TABLE IF NOT EXISTS stores_meta (
   timestamp        int,
   store_id         int NOT NULL,
   bikoret_no       int,
@@ -78,6 +77,7 @@ CREATE TABLE stores_meta (
   last_update_date text,
   last_update_time text
 );
+DELETE FROM stores_meta;
 COMMENT ON TABLE stores_meta IS 'Metadata about stores. Each store may appear several times.';
 COMMENT ON COLUMN stores_meta.timestamp IS 'Unix time when this entry was encountered. (safe)';
 COMMENT ON COLUMN stores_meta.store_id IS 'References stores.store_id. (safe)';
@@ -86,12 +86,13 @@ COMMENT ON COLUMN stores_meta.store_type IS '1 for physical, 2 for online, 3 for
 
 COPY stores_meta FROM '/home/amit/prices/data_parsed/stores_meta.txt' WITH (FORMAT csv);
 
-CREATE TABLE items (
+CREATE TABLE IF NOT EXISTS items (
   item_id    integer,
   item_type  int   NOT NULL,
   item_code  text  NOT NULL,
   chain_id   text
 );
+DELETE FROM items;
 COMMENT ON TABLE items IS 'Identifies every commodity item in the data. Each item may appear once.';
 COMMENT ON COLUMN items.item_id IS '(safe)';
 COMMENT ON COLUMN items.item_type IS '0 for internal codes, 1 for barcodes.';
@@ -100,7 +101,7 @@ COMMENT ON COLUMN items.chain_id IS 'Empty string for universal.';
 
 COPY items FROM '/home/amit/prices/data_parsed/items.txt' WITH (FORMAT csv);
 
-CREATE TABLE items_meta (
+CREATE TABLE IF NOT EXISTS items_meta (
   timestamp                     int,
   item_id                       int  NOT NULL,
   chain_id                      text NOT NULL,
@@ -113,6 +114,7 @@ CREATE TABLE items_meta (
   allow_discount                text,
   item_status                   text
 );
+DELETE FROM items_meta;
 COMMENT ON TABLE items_meta IS 'Contains all metadata about each item. Each item may appear several times.';
 COMMENT ON COLUMN items_meta.timestamp IS 'Unix time when this entry was encountered. (safe)';
 COMMENT ON COLUMN items_meta.item_id IS 'References items.item_id. (safe)';
@@ -124,7 +126,7 @@ COMMENT ON COLUMN items_meta.item_status IS '???';
 
 COPY items_meta FROM '/home/amit/prices/data_parsed/items_meta.txt' WITH (FORMAT csv);
 
-CREATE TABLE prices (
+CREATE TABLE IF NOT EXISTS prices (
   timestamp             int,
   item_id               int NOT NULL,
   store_id              int NOT NULL,
@@ -133,6 +135,7 @@ CREATE TABLE prices (
   unit_of_measure       text,
   quantity              text
 );
+DELETE FROM prices;
 COMMENT ON TABLE prices IS 'Contains all reported prices for all items.';
 COMMENT ON COLUMN prices.timestamp IS 'Unix time when this entry was encountered. (safe)';
 COMMENT ON COLUMN prices.item_id IS 'References items.item_id. (safe)';
@@ -144,7 +147,7 @@ COMMENT ON COLUMN prices.quantity IS 'How many grams/liters etc.';
 
 COPY prices FROM '/home/amit/prices/data_parsed/prices.txt' WITH (FORMAT csv);
 
-CREATE TABLE promos (
+CREATE TABLE IF NOT EXISTS promos (
   promo_id                     integer,
   timestamp_from               int,
   timestamp_to                 int,
@@ -174,6 +177,7 @@ CREATE TABLE promos (
   number_of_items              int,
   not_in_promos_items          int
 );
+DELETE FROM promos;
 COMMENT ON TABLE promos IS 'Identifies every promotion in the data. Promo id and metadata are saved together since they are unique. A change in the metadata will be registered as a new promo.';
 COMMENT ON COLUMN promos.promo_id IS '(safe)';
 COMMENT ON COLUMN promos.timestamp_from IS 'Unix time when this entry was first encountered. (safe)';
@@ -195,21 +199,23 @@ COMMENT ON COLUMN promos.not_in_promos_items IS '0 if reported in promos_items, 
 
 COPY promos FROM '/home/amit/prices/data_parsed/promos.txt' WITH (FORMAT csv);
 
-CREATE TABLE promos_stores (
+CREATE TABLE IF NOT EXISTS promos_stores (
   promo_id int NOT NULL,
   store_id int NOT NULL
 );
+DELETE FROM promos_stores;
 COMMENT ON TABLE promos_stores IS 'Reports what stores take part in every promo. A single promo may have several rows, one for each store.';
 COMMENT ON COLUMN promos_stores.promo_id IS 'References promos.promo_id. (safe)';
 COMMENT ON COLUMN promos_stores.store_id IS 'References stores.store_id. (safe)';
 
 COPY promos_stores FROM '/home/amit/prices/data_parsed/promos_stores.txt' WITH (FORMAT csv);
 
-CREATE TABLE promos_items (
+CREATE TABLE IF NOT EXISTS promos_items (
   promo_id     int NOT NULL,
   item_id      int NOT NULL,
   is_gift_item text
 );
+DELETE FROM promos_items;
 COMMENT ON TABLE promos_items IS 'Reports what items take part in every promo. A single promo may have several rows, one for each item.
 
 CAVEAT: promos that include more than 100 items are not reported here, because those promos usually apply on an entire store ("everything for 10% discount") and that bloats the DB. They are reported on the other tables as usual.';
@@ -218,7 +224,7 @@ COMMENT ON COLUMN promos_items.item_id IS 'References items.item_id. (safe)';
 
 COPY promos_items FROM '/home/amit/prices/data_parsed/promos_items.txt' WITH (FORMAT csv);
 
-CREATE /* TEMPORARY */ TABLE promos_to (
+CREATE TEMP TABLE promos_to (
 -- A temporary table for updating the timestamp_to field in promos. The
 -- timestamp_to field is evaluated after reporting each promo, so it has to be
 -- updated separately.
@@ -232,5 +238,4 @@ UPDATE promos SET timestamp_to = (
   SELECT max(timestamp_to) FROM promos_to
   WHERE promos_to.promo_id = promos.promo_id
 );
-DROP TABLE promos_to;
 
